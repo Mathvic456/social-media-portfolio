7e441d8947f7ee0019349689066f23b4401e1fd5
Victor Matthew <vmatthew727@gmail.com>
2025-06-26 04:01:00 +0100

    Init

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: "Kutie's Portfolio",
+  description: 'Kutie is a social media influencer and content creator.',
+  // generator: 'Kutie',
 }
 
 export default function RootLayout({
